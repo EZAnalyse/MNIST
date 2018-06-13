bcb8dd38a256273e34cecb205c708ab98e5c775e
Dreamix <pengchun1729@gmail.com>
2018-06-13 20:38:42 +0800

    fix bug

--- db_explore.py
+++ db_explore.py
@@ -3,13 +3,12 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
 
 # load the mnist database from the *.npy files
 def load_mnist(path='./mnist_db/'):
     # numpy.load for .npy file
-    # return numpy.array
+    # return numpy.array, (train_db, test_db)
     # x: image
     # y: label
     x_train = np.load(path+'x_train.npy')
@@ -20,7 +19,7 @@ def load_mnist(path='./mnist_db/'):
 
 
 # show image by random
-def rnd_show(images, labels):
+def __rnd_show(images, labels):
     # get a random index
     # plt.show for img in np.array type
     index = np.random.randint(len(images))
@@ -34,8 +33,7 @@ def rnd_show(images, labels):
 def main():
     train, test = load_mnist()
     x_train, y_train = train
-    # ds = tf.data.Dataset.from_tensor_slices(x_train)
-    rnd_show(x_train, y_train)
+    __rnd_show(x_train, y_train)
 
 
 if __name__ == '__main__':
